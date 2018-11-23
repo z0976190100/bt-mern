8c0b2ac5266f60555e9c7638e959229a44bac549
z0976190100 <z0976190100@gmail.com>
2018-11-23 03:36:55 +0200

    deleteContact route for profiles

--- models/Profile.js
+++ models/Profile.js
@@ -2,23 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
-        _user: {
-            type: Schema.Types.ObjectId,
-            ref: "users" // "users" ?
-        },
-        handle: {
-            type: String,
-            required: true,
-            max: 40
-        },
-        employment: {
-            type: String,
-            required: true
-        },
-        phonenumber: {
-            type: String,
-            default: "no phonenumber"
+    _user: {
+        type: Schema.Types.ObjectId,
+        ref: "users" // "users" ?
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    employment: {
+        type: String,
+        required: true
+    },
+    contacts: [
+        {
+            phonenumber: {
+                type: String,
+                default: "no phonenumber"
+            },
+
         }
-    });
+    ]
+});
 
 module.exports = Profile = mongoose.model("profiles", ProfileSchema);
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -9,7 +9,7 @@ const getHelpers = {
     currentProfile: (req, res) => {
         const {id} = req.user;
         Profile.findOne({_user: id})
-            .populate("_user", ["name", "avatar"])
+            .populate("_user", ["name", "avatar"]) // fetching data from bind schema
             .then(profile => {
                 if (!profile) return res.status(404).json({profile: "Profile not found."});
                 res.json(profile);
@@ -20,12 +20,40 @@ const getHelpers = {
                         ${err}`);
                 return res.status(400).json(err);
             });
-    }
+    },
+    withHandleProfile: (req, res) => {
+        Profile.findOne({handle: req.params.handle})
+            .populate("_user", ["name", "avatar"]) // fetching data from bind schema
+            .then(profile => {
+                if (!profile) return res.status(404).json({profile: "Profile not found."});
+                res.json(profile);
+            })
+            .catch(err => {
+                console.log(
+                    `\n < profiles.js:31 > ERROR: IN withHandleProfile. While getting Profile we've got \n
+                        ${err}`);
+                return res.status(400).json(err);
+            });
+    },
+    userIdProfile: (req, res) => {
+        Profile.findOne({handle: req.params.id})
+            .populate("_user", ["name", "avatar"]) // fetching data from bind schema
+            .then(profile => {
+                if (!profile) return res.status(404).json({profile: "Profile not found."});
+                res.json(profile);
+            })
+            .catch(err => {
+                console.log(
+                    `\n < profiles.js:31 > ERROR: IN userIdProfile. While getting Profile we've got \n
+                        ${err}`);
+                return res.status(400).json(err);
+            });
+    },
 };
 
 const postHelpers = {
     saveProfile: (req, res) => {
-        //validation of received data
+        //TODO: validation of received data
         // const {errors, isValid} = validateRegistrationData(req.body);
         // if (!isValid) {
         //     return res.status(400).json(errors);
@@ -44,14 +72,13 @@ const postHelpers = {
                     });
                     return res.status(400).json(errors);
                 } else {
-                    const {handle, employment, phonenumber} = req.body;
+                    const {handle, employment } = req.body;
                     // create new Profile and save it to DB
                     const newProfile = new Profile(
                         {
                             _user: id,
                             handle,
-                            employment,
-                            phonenumber
+                            employment
                         }
                     );
                     newProfile.save()
@@ -60,16 +87,64 @@ const postHelpers = {
                         })
                         .catch(err => {
                             console.log(
-                                `\n < profiles.js:60 > ERROR: IN saveProfile. While saving newProfile we've got \n
+                                `\n < profiles.js:89 > ERROR: IN saveProfile. While saving newProfile we've got \n
                         ${err}`);
                             return res.status(400).json(err);
                         });
 
                 }
             })
+    },
+    addContactsProfile: (req, res) => {
+        //TODO: VALIDATION
+        Profile.findOne({_user: req.user.id})
+            .then(profile => {
+                const newContact = {
+                    phonenumber: req.body.phonenumber
+                };
+
+                profile.contacts.unshift(newContact);
+
+                profile.save()
+                    .then(profile =>
+                        res.json(profile));
+            });
     }
 };
 
+const deleteHelpers = {
+    deleteContact: (req, res) => {
+        const index = req.params.cont_id;
+        Profile.findOne({_user: req.user.id})
+            .then(profile => {
+                const removeIndex = profile.contacts
+                    .map(item => item.id)
+                    .indexOf(index);
+
+                profile.contacts.splice(removeIndex, 1);
+
+                profile.save()
+                    .then(profile =>
+                        res.json(profile));
+
+            });
+    }
+};
+
+
+// @route GET api/profiles/handle/:handle
+// @desc returns profile for handle bind to user ( for public use )
+// @access Public
+router.get(
+    "/handle/:handle",
+    (req, res) => getHelpers.withHandleProfile(req, res));
+
+// @route GET api/profiles/users/:user_id
+// @desc returns profile for handle bind to user ( for public use )
+// @access Public
+router.get(
+    "/users/:id",
+    (req, res) => getHelpers.userIdProfile(req, res));
 
 // @route GET api/profiles/current
 // @desc returns profile for current logged user
@@ -86,6 +161,22 @@ router.post(
     "/",
     passport.authenticate("jwt", {session: false}),
     (req, res) => postHelpers.saveProfile(req, res));
+
+// @route POST api/profiles/contacts/
+// @desc add contacts array to exact Profile for exact user
+// @access Private
+router.post(
+    "/contacts",
+    passport.authenticate("jwt", {session: false}),
+    (req, res) => postHelpers.addContactsProfile(req, res));
+
+// @route DELETE api/profiles/contacts/:index
+// @desc removes contact from array in exact Profile for exact user
+// @access Private
+router.delete(
+    "/contacts/:cont_id",
+    passport.authenticate("jwt", {session: false}),
+    (req, res) => deleteHelpers.deleteContact(req, res));
 
 // @route GET api/profiles/test
 // @desc tests route
